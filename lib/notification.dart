59c517544dea5e0b8b1463182396b54c40f3ade1
ljh-none <144912155+ljh-none@users.noreply.github.com>
2024-05-30 02:40:41 +0900

    add fcm notification

--- lib/notification.dart
+++ lib/notification.dart
@@ -18,44 +18,50 @@ Future<void> listenFcmInBackground(RemoteMessage message) async {
   //app이 background & terminated된 상태에서 fcm메시지 처리
   debugPrint("[log] got a fcm msg in background");
 
-  if (message.notification != null) {
-    debugPrint('[log] Got a notification in message');
-    final backNotiController = FlutterLocalNotificationsPlugin();
-    var notiDetailForAnd = const AndroidNotificationDetails(
-      'your channel id',
-      'your channel name',
-      channelDescription: 'your channel description',
-      importance: Importance.max,
-      priority: Priority.high,
-      ticker: 'ticker',
-    );
-    var notiDetail = NotificationDetails(android: notiDetailForAnd);
+  final backNotiController = FlutterLocalNotificationsPlugin();
+  debugPrint('[log] Got a notification in message');
+  var notiDetailForAnd = const AndroidNotificationDetails(
+    'your channel id',
+    'your channel name',
+    channelDescription: 'your channel description',
+    importance: Importance.max,
+    priority: Priority.high,
+    ticker: 'ticker',
+  );
+  var notiDetail = NotificationDetails(android: notiDetailForAnd);
 
-    await backNotiController.show(
-      1,
-      message.notification!.title,
-      message.notification!.body,
-      notiDetail,
-      payload: 'item x',
-    );
-  }
+  await backNotiController.show(
+    1,
+    message.data['title'],
+    message.data['body'],
+    notiDetail,
+    payload: 'item x',
+  );
 }
 
 class FcmNotification {
+  static late FlutterLocalNotificationsPlugin notiController;
+  static late StreamController<String?> notiTapStream;
+  static late AndroidNotificationChannel notiChannel;
+
+  static int notiId = 1; //개별 푸쉬알림 ID
+  static String notiChId = 'ntCh'; //알림 채널 ID
+  static String notiStreamId = 'ntStream'; //알림스트림(리스너) ID
+
   static String? _fcmToken;
 
   String? get fcmToken => _fcmToken;
 
   static Future<bool> initFcmNotification() async {
+    //get fcm token
     _fcmToken = await FirebaseMessaging.instance.getToken();
     if (_fcmToken == null) {
       debugPrint('get fcmToken failed');
       return false;
     }
-
     debugPrint('[log] fcm token : $_fcmToken');
 
-    //fcmToken 변경 감지 함수
+    //fcmToken 변경 시 핸들링
     FirebaseMessaging.instance.onTokenRefresh.listen((fcmToken) {
       debugPrint('[log] fcm token updated!!');
     }).onError((err) {
@@ -65,7 +71,37 @@ class FcmNotification {
 
     //fcm message를 백그라운드 및 종료 상태에서도 감지하도록 설정
     FirebaseMessaging.onBackgroundMessage(listenFcmInBackground);
+
+    //init local notification
+    notiTapStream = StreamController<String?>.broadcast(); //listener
+    notiChannel = AndroidNotificationChannel(
+      notiChId, // id
+      'MY FOREGROUND SERVICE', // title
+      description: 'notification ch',
+      importance: Importance.low,
+    );
+    notiController = FlutterLocalNotificationsPlugin(); //controller
+    await notiController.initialize(
+      const InitializationSettings(
+        android: AndroidInitializationSettings('app_icon'),
+      ),
+      onDidReceiveNotificationResponse: onNotiTapped,
+      //onDidReceiveBackgroundNotificationResponse: interactNotiWithoutTap,
+    );
+
+    //get permission
+    getNotiPermission();
+    getFcmPermission();
+
     return true;
+  }
+
+  static void getNotiPermission() {
+    FlutterLocalNotificationsPlugin()
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()!
+        .requestNotificationsPermission();
+    debugPrint('[log] User granted notification permission');
   }
 
   static Future<void> getFcmPermission() async {
@@ -84,6 +120,13 @@ class FcmNotification {
     debugPrint('[log] ${settings.authorizationStatus}');
   }
 
+  static void onNotiTapped(NotificationResponse notificationResponse) {
+    debugPrint('[log] notification tapped');
+    //리스너 대기열(스트림)에 추가
+    notiTapStream.add(notificationResponse.payload);
+  }
+
+  //얘는 일단 보류해두고.
   static Future<void> setTapListener(BuildContext context) async {
     //when app terminated
     await FirebaseMessaging.instance.getInitialMessage().then((initialMessage) {
